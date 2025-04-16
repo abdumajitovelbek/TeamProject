9466b917e0e82c1f3204c3bee66c622d0faea131
abdumajitovelbek <abdumajitovelbek1@gmail.com>
2025-04-16 10:15:08 +0500

    Update RegularUser.h

--- RegularUser.h
+++ RegularUser.h
@@ -6,13 +6,14 @@
 #include "User.h"
 #include "RoadReport.h"
 
-
-
 class RegularUser : public User {
 public:
     virtual ~RegularUser() {}
     RegularUser(string name, int id, string email);
     void displayInfo() override;
+    string getName() const override;   
+    string getEmail() const override;  
+    int getID() const override;        
     RoadReport createReport(string location, string description, Severity severity);
     void supportReport(int reportID);
 };
