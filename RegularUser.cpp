b08406d54222b710dbd6e1ed4e97f134f455385d
abdumajitovelbek <abdumajitovelbek1@gmail.com>
2025-04-16 10:14:34 +0500

    Update RegularUser.cpp

--- RegularUser.cpp
+++ RegularUser.cpp
@@ -4,17 +4,28 @@
 #include <iostream>
 #include "Utils.h"
 
-
 RegularUser::RegularUser(string name, int id, string email) : User(name, id, email) {}
 
 void RegularUser::displayInfo() {
     cout << "Regular User: " << username << " (ID: " << userID << ", Email: " << email << ")" << endl;
 }
 
+string RegularUser::getName() const {
+    return getUsername();
+}
+
+string RegularUser::getEmail() const {
+    return email;
+}
+
+int RegularUser::getID() const {
+    return getUserID();
+}
+
 RoadReport RegularUser::createReport(string location, string description, Severity severity) {
-    return RoadReport(userID, location, description, severity, getCurrentDate());
+    return RoadReport(0, userID, location, description, severity, getCurrentDate()); // reportID set in main.cpp
 }
 
 void RegularUser::supportReport(int reportID) {
-    cout << "User " << userID << " supported report ID " << reportID << endl;
+    cout << "User " << userID << " supported report ID " << reportID << endl; // Optional, can be removed
 }
