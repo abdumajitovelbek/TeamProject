a5b6aca3ea08442d2cf1020dc50d3a6567500d64
abdumajitovelbek <abdumajitovelbek1@gmail.com>
2025-04-16 10:05:35 +0500

    Update Admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -2,10 +2,23 @@
 #include "Admin.h"
 #include <iostream>
 
-Admin::Admin(string name, int id, string email, ReportManager* mgr) : User(name, id, email), manager(mgr) {}
+Admin::Admin(string name, int id, string email, ReportManager* mgr)
+    : User(name, id, email), manager(mgr) {}
 
 void Admin::displayInfo() {
     cout << "Admin: " << username << " (ID: " << userID << ", Email: " << email << ")" << endl;
+}
+
+string Admin::getName() const {
+    return getUsername(); 
+}
+
+string Admin::getEmail() const {
+    return email; 
+}
+
+int Admin::getID() const {
+    return getUserID(); 
 }
 
 void Admin::viewAllReports() {
